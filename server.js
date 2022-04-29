e5be3d6a750d779d1f1b3fea2648722845b547dc
Alex Glaubitz <e.pirazzi@gmail.com>
2022-04-29 13:45:28 -0400

    html pages loading

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes/notes.js');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+
+// parse incoming JSON data
+app.use(express.json());
+
+// make css and js files readily available
+app.use(express.static(__dirname + '/public'));
+
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
