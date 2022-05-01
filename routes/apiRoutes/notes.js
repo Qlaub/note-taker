e862cdfe620d1f86882b8a7b4b86478dac5a60f7
Alex Glaubitz <e.pirazzi@gmail.com>
2022-05-01 18:32:28 -0400

    router.delete functioning

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,23 +1,30 @@
 const router = require('express').Router();
 const {v4: uuidv4} = require('uuid');
 const notes = require('../../db/db.json');
-const createNewNote = require('../../lib/notes');
+const {createNewNote, deleteNote} = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
   let results = notes;
-  console.log(results);
 
   res.json(results);
 });
 
 router.post('/notes', (req, res) => {
-  console.log(req.body);
   // set id to a unique string using the UUID package
   req.body.id = uuidv4();
 
   // add new note to notes array and write to db.json file
   const note = createNewNote(req.body, notes);
   res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+  // id of note to be deleted
+  const id = req.params.id;
+
+  deleteNote(id, notes);
+
+  res.send(true);
 });
 
 module.exports = router;
